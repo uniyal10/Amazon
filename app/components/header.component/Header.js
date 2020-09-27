1caf95ddb673eb0a0de638abc67a1abaedc088bf
sudhanshu uniyal <sudhanshu.uniyal@sourcefuse.com>
2020-09-27 20:12:10 +0530

    header completed suessfully

--- app/components/header.component/Header.js
+++ app/components/header.component/Header.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react"
 import { Link } from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
   return (
     <>
@@ -8,7 +10,41 @@ function Header() {
           <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
         </Link>
         {/* Search box */}
-        <input type="text" className="header__searchInput" />
+        <div className="header__search">
+          <input type="text" className="header__searchInput" />
+          <SearchIcon className="header__searchIcon"></SearchIcon>
+        </div>
+        <div className="header__nav">
+
+          <Link to="/login" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Hello kapil</span>
+              <span className="header__optionLineTwo">& Sign In</span>
+            </div>
+          </Link>
+          <Link to="/" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Returns</span>
+              <span className="header__optionLineTwo">& Orders</span>
+            </div>
+
+          </Link>
+          <Link to="/" className="header__link">
+            <div className="header__option">
+              <span className="header__optionLineOne">Your</span>
+              <span className="header__optionLineTwo">Prime</span>
+            </div>
+          </Link>
+
+          <Link to="/checkout" className="header__link">
+            <div className="header__optionBasket">
+              <ShoppingBasketIcon />
+              <div className="header__optionLineTwo header__basket">0</div>
+            </div>
+          </Link>
+
+        </div>
+
       </nav>
     </>
   )
